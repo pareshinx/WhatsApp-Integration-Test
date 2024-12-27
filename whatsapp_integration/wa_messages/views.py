1e275a40a0eefdd1b8bfba33f086dd825fade027
Paresh Malukani <pareshm.inexture@gmail.com>
2024-12-27 15:25:04 +0530

    feat: created an API to send the message to user.

--- whatsapp_integration/wa_messages/views.py
+++ whatsapp_integration/wa_messages/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import os
 
+from django.http.response import JsonResponse
+from rest_framework.views import APIView
+import requests
+
+WHATSAPP_PHONE_ID = os.getenv('WHATSAPP_PHONE_ID')
+WHATSAPP_API_ACCESS_TOKEN = os.getenv('WHATSAPP_API_ACCESS_TOKEN')
+META_API_DOMAIN = os.getenv('META_API_DOMAIN')
 # Create your views here.
+class SendMessage(APIView):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        phone_number = request.data.get('phone_number')
+        message = request.data.get('message')
+
+        api_url = f"{META_API_DOMAIN}/{WHATSAPP_PHONE_ID}/messages"
+        payload = {
+            "messaging_product": "whatsapp",
+            "to": phone_number,
+            "type": "text",
+            "text": {
+                "body": message,
+            }
+        }
+
+        headers = {
+            "Authorization": f"Bearer {WHATSAPP_API_ACCESS_TOKEN}",
+            "Content-Type": "application/json"}
+
+        response = requests.post(api_url, headers=headers, json=payload)
+
+        return JsonResponse(response.json())
--- whatsapp_integration/whatsapp_integration/settings.py
+++ whatsapp_integration/whatsapp_integration/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#jk42y2oxghi1+z1057^tchr8o7!571yi!by&iqs5h5i4y@h#='
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
